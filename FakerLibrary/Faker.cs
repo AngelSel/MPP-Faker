8502ab824c56de2d3ca883df73054f55dbc20bd1
AngelSel <angelbox1@gmail.com>
2020-10-05 21:25:01 +0300

    The logic of FakerClass & Generator interface have been added

--- FakerLibrary/Faker.cs
+++ FakerLibrary/Faker.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace FakerLibrary
 {
@@ -11,6 +13,69 @@ namespace FakerLibrary
         }
 
         private object Create(Type t) 
+        {
+            if(t.IsPrimitive)
+            {
+                return CreatePrimitive(t);
+            }
+            else if(t.IsClass)
+            {
+                return CreateClassObject(t);
+
+            }
+            else if(t.IsValueType)
+            {
+                return CreateStructure(t);
+            }
+            throw new Exception("Cant create new object");
+        }
+
+        private object CreatePrimitive(Type type)
+        {
+
+        }
+
+        private object CreateClassObject(Type type)
+        {
+            ConstructorInfo[] currentConstructors = type.GetConstructors();
+            object[] constructorParams = null;
+            ConstructorInfo chosenConstructor = null;
+            object createdClassObject = default;
+            
+            foreach (ConstructorInfo cInfo in currentConstructors.OrderByDescending(c => c.GetParameters().Length))
+            {
+                constructorParams = GenerateConstructorsParams(cInfo);
+                try
+                {
+                    createdClassObject = cInfo.Invoke(constructorParams);
+                    chosenConstructor = cInfo;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+
+            GenerateFieldsAndProperties(createdClassObject);
+            return createdClassObject;
+        }
+        private object CreateStructure(Type type)
+        {
+           
+        }
+
+        private object[] GenerateConstructorsParams(ConstructorInfo cInfo)
+        {
+            ParameterInfo[] paramsInfo = cInfo.GetParameters();
+            object[] generatedParams = new object[paramsInfo.Length];
+
+            for(int i=0; i < generatedParams.Length; i++)
+            {
+
+            }
+        }
+
+        private void GenerateFieldsAndProperties(object createdObject)
         {
 
         }
