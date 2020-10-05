50ef5bf3478e4111862bfb510b83e250b7e8bbbf
AngelSel <angelbox1@gmail.com>
2020-10-05 18:31:07 +0300

    Creating the project

---
+++ FakerLibrary/Faker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FakerLibrary
+{
+    public class Faker
+    {
+
+        public T Create<T>() 
+        {
+            return (T)Create(typeof(T));
+        }
+
+        private object Create(Type t) 
+        {
+
+        }
+
+    }
+}
