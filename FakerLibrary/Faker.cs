1b5348afdb05ba333e84147759e33cdfbd45599a
AngelSel <angelbox1@gmail.com>
2020-10-13 23:07:18 +0300

    Fix minor bugs

--- FakerLibrary/Faker.cs
+++ FakerLibrary/Faker.cs
@@ -9,7 +9,18 @@ namespace FakerLibrary
 {
     public class Faker
     {
-        private List<IGenerator> gens;
+        private List<IGenerator> gens = new List<IGenerator>
+        {
+           new ListGenerator(),
+           new BoolGenerator(),
+           new DataGenerator(),
+           new DoubleGenerator(),
+           new FloatGenerator(),
+           new LongGenerator(),
+           new ShortGenerator(),
+           new StringGenerator()
+        };
+
         public int MaxCircularDependency { get; set; } = 0;
         public int currentCircularDependency = 0;
         public Stack<Type> constructionStack = new Stack<Type>();
@@ -18,17 +29,9 @@ namespace FakerLibrary
 
         public Faker()
         {
-            this.gens = LoadGenerators();
-            gens.Add(new ListGenerator());
-            gens.Add(new BoolGenerator());
-            gens.Add(new DataGenerator());
-            gens.Add(new DoubleGenerator());
-            gens.Add(new FloatGenerator());
-            gens.Add(new LongGenerator());
-            gens.Add(new ShortGenerator());
-            gens.Add(new StringGenerator());
             random = new Random();
-
+            LoadGenerators();            
+          
         }
 
         public T Create<T>() 
@@ -138,7 +141,6 @@ namespace FakerLibrary
 
         private void GenerateFieldsAndProperties(object createdObject, ParameterInfo[] ctorParams, ConstructorInfo cInfo)
         {
-            //ParameterInfo[] pInfo = cInfo?.GetParameters();
             var fields = createdObject.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public).Cast<MemberInfo>();
             var properties = createdObject.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public).Cast<MemberInfo>();
             var fieldsAndProperties = fields.Concat(properties);
@@ -181,9 +183,8 @@ namespace FakerLibrary
         }
 
 
-        private List<IGenerator> LoadGenerators()
+        private void LoadGenerators()
         {
-            List<IGenerator> loadedGenerators = new List<IGenerator>();
 
             string pluginsPath = @"d:\Ангелина\5 сем\5 сем\СПП\Lab2-MPP\MPP-Faker\pl";
             //string pluginsPath = Directory.GetCurrentDirectory() + @"\Plugins";
@@ -197,18 +198,10 @@ namespace FakerLibrary
                      if (IsRequiredType(t, typeof(Generator<>)))
                      {
                          var currentGenerator = Activator.CreateInstance(t);
-                         loadedGenerators.Add((IGenerator)currentGenerator);
+                         gens.Add((IGenerator)currentGenerator);
                      }
                  }
             } 
-/*
-            foreach (Type t in Assembly.GetExecutingAssembly().GetTypes())
-            {
-                if(IsRequiredType(t, typeof(Generator<>)))
-                    loadedGenerators.Add((IGenerator)Activator.CreateInstance(t));
-            }*/
-
-            return loadedGenerators;
         }
 
         private bool IsRequiredType(Type current, Type isRequired)
